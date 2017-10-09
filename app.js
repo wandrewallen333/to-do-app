4629176258fb63d56c7e38bba9593509d2861766
Andrew Allen <w.andrewallen@gmail.com>
2017-10-09 17:59:06 -0600

    Revision to Checkpoint 7

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
+//jshint esnext:true
 function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoList = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
 
-  addToDoForm.addEventListener('submit', () => {
+
+  addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
-
     let title = newToDoText.value;
-
     let newLi = document.createElement('li');
-
     let checkbox = document.createElement('input');
-
+    let removeTask = document.createElement('input');
+    removeTask.type = "button";
+    removeTask.value = "Delete"
     checkbox.type = "checkbox";
-
     newLi.textContent = title;
-
     newLi.appendChild(checkbox);
-
+    newLi.appendChild(removeTask);
     toDoList.appendChild(newLi);
+    newToDoList.value = '';
 
-    newToDoText.value = ''; 
+  removeTask.addEventListener('click', function(e) {
+    newLi.parentNode.removeChild(newLi);
+    newLi.appendChild(removeTask);
+  });
+
 
   });
+
 }
 
 window.onload = function() {
